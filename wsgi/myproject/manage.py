60ea85c959b4d8c457b067e7e5c8375c7b5af6ff
Alexandru Cristian <alex.cristian@drexel.edu>
2016-05-26 09:43:01 -0400

    Initial OpenShift commit.

--- wsgi/myproject/manage.py
+++ wsgi/myproject/manage.py
@@ -3,7 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CI103.settings")
+    # GETTING-STARTED: change 'myproject' to your project name:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
     from django.core.management import execute_from_command_line
 
